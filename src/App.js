84f3da539e229b3cddab1e6fa37d1c110c16b2a5
richardingwe <richardingwe5@gmail.com>
2021-11-17 08:41:36 +0100

    added dynamic data to the list

--- src/App.js
+++ src/App.js
@@ -6,9 +6,42 @@ function App() {
   const [toAccountNumber, setToAccountNumber] = useState('');
   const [dropdownVisible, setDropdownVisble] = useState(false);
 
-  const handleItem = (e) => {
+  const data = [
+    {
+      name: 'Maria Ingwe',
+      bank: 'First Bank',
+      accountNumber: 1234567890,
+    },
+    {
+      name: 'John Doe',
+      bank: 'GT Bank',
+      accountNumber: 2222222222,
+    },
+    {
+      name: 'Isong King',
+      bank: 'Access Bank',
+      accountNumber: 3333333333,
+    },
+    {
+      name: 'Miriam Stone',
+      bank: 'Stanbic IBTC Bank',
+      accountNumber: 4345454343,
+    },
+    {
+      name: 'Angella Bone',
+      bank: 'Kuda Bank',
+      accountNumber: 9584373892,
+    },
+    {
+      name: 'Lizzy Williams',
+      bank: 'Diamond Bank',
+      accountNumber: 2984637899
+    },
+  ];
+
+  const handleItem = (data) => {
     console.log('clicked');
-    setToAccountNumber('1234567890');
+    setToAccountNumber(data.accountNumber);
   };
 
   const handleInputFocus = (e) => {
@@ -46,54 +79,16 @@ function App() {
           </svg>
 
           <div className='beneficiary_dropdown' style={{ display: dropdownVisible ? 'block' : '' }}>
-            <div className="item" onClick={handleItem}>
-              <div className="name">Maria Ingwe</div>
-              <div className='bank-details'>
-                First Bank: 1234567890
-              </div>
-            </div>
-            <div className="item" onClick={handleItem}>
-              <div className="name">Maria Ingwe</div>
-              <div className='bank-details'>
-                First Bank: 1234567890
-              </div>
-            </div>
-            <div className="item" onClick={handleItem}>
-              <div className="name">Maria Ingwe</div>
-              <div className='bank-details'>
-                First Bank: 1234567890
-              </div>
-            </div>
-            <div className="item" onClick={handleItem}>
-              <div className="name">Maria Ingwe</div>
-              <div className='bank-details'>
-                First Bank: 1234567890
-              </div>
-            </div>
-            <div className="item" onClick={handleItem}>
-              <div className="name">Maria Ingwe</div>
-              <div className='bank-details'>
-                First Bank: 1234567890
-              </div>
-            </div>
-            <div className="item" onClick={handleItem}>
-              <div className="name">Maria Ingwe</div>
-              <div className='bank-details'>
-                First Bank: 1234567890
-              </div>
-            </div>
-            <div className="item" onClick={handleItem}>
-              <div className="name">Maria Ingwe</div>
-              <div className='bank-details'>
-                First Bank: 1234567890
-              </div>
-            </div>
-            <div className="item" onClick={handleItem}>
-              <div className="name">Maria Ingwe</div>
-              <div className='bank-details'>
-                First Bank: 1234567890
-              </div>
-            </div>
+            {
+              data.map(data => (
+                <div className="item" onClick={() => handleItem(data)}>
+                  <div className="name">{data.name}</div>
+                  <div className='bank-details'>
+                    {data.bank}: {data.accountNumber}
+                  </div>
+                </div>
+              ))
+            }
           </div>
         </div>
       </div>
